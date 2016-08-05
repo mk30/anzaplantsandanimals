38c90d91c633e0ced206c075c272e878f337cb05
Marina Kukso <marina.kukso@gmail.com>
2016-08-04 20:39:23 -0700

    callbacks in the right place for add and showall

--- index.js
+++ index.js
@@ -6,20 +6,16 @@ var init = fs.readFileSync('./init.sql', 'utf8')
 exports.init = function () {
   db.run(init)
 }
-exports.add = function () {
+exports.add = function (cb) {
   db.serialize(function() {
     var stmt = db.prepare("INSERT INTO samples (dateobserved, location) VALUES (?, ?)")
     stmt.run("jan 1", "derreen")
-    stmt.finalize()
-    db.close()
+    stmt.finalize(cb)
   })
 }
-exports.show = function (){
+exports.showall = function (cb){
   db.serialize(function() {
-    db.each("SELECT sampleid, dateobserved, location FROM samples", function(err, row) {
-      console.log(row.sampleid + ": " + row.dateobserved +
-      ": " + row.location)
-    })
+    db.all("SELECT sampleid, dateobserved, location FROM samples", cb)
   })
   db.close()
 }
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var api = require('./')
 //api.init()
-//api.add()
-api.show()
+api.add(function (error){
+  if (error) return console.error('error: ', error)
+  api.showall(
+    function (error, data) {
+      if (error) return console.error('error: ', error)
+      console.log(data)
+    }
+  )
+})
